16ce6409eb40db6c832a3c0d4374e009a07ef583
arevel3 <0Zq+C.96U^I)@6>
2025-11-12 10:59:04 -0500

    add secury header

--- app.py
+++ app.py
@@ -36,6 +36,14 @@ def create_app(config_class=Config):
     app.register_blueprint(delete_bp)
     app.register_blueprint(rerun_bp)
     
+    # Add security headers
+    @app.after_request
+    def add_security_headers(response):
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        response.headers['X-Frame-Options'] = 'DENY'
+        response.headers['X-XSS-Protection'] = '1; mode=block'
+        return response
+    
     return app
 
 
